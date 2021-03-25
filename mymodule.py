88859ce281428f0b340d6e81b8135d77cdd06f15
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2021-03-25 19:32:58 +0530

    Day12

---
+++ mymodule.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def generate_full_name(firstname, lastname):
+      space = ' '
+      fullname = firstname + space + lastname
+      return fullname
+
+def sum_two_nums (num1, num2):
+    return num1 + num2
+gravity = 9.81
+person = {
+    "firstname": "Ayush",
+    "age": 21,
+    "country": "India",
+    "city":'Kaputhala'
+}
+
